467a431cfe3f0edf9035742e5bed21efb001c5b3
Sin Shimozono <sin@magic.odn.ne.jp>
2013-04-03 01:50:40 +0900

    e407 board

---
+++ STM32Libraries/armcmx/utility/olimex_stm_e407.h
@@ -0,0 +1,27 @@
+#ifndef _OLIMEX_STM32E407_H_
+#define _OLIMEX_STM32E407_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "armcmx.h"
+#include "stm_eval_compat.h"
+
+#ifndef OLIMEX_STM_E407
+#define OLIMEX_STM_E407
+#endif
+#define STDSERIAL 3
+	
+#define LED1        PC13
+#define LED2        NOT_A_PIN
+#define LED3        NOT_A_PIN
+#define LED4        NOT_A_PIN
+#define BUTTON1 		PA0
+	
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
---
+++ STM32Libraries/armcmx/utility/stm_eval_compat.h
@@ -0,0 +1,16 @@
+#include "stm32f4xx.h"
+#include "armcmx.h"
+
+typedef GPIOPin Led_TypeDef;
+typedef GPIOPin Button_TypeDef;
+typedef GPIOMode_TypeDef ButtonMode_TypeDef;
+typedef usart* COM_TypeDef;
+
+
+void STM_EVAL_LEDInit(Led_TypeDef Led);
+void STM_EVAL_LEDOn(Led_TypeDef Led);
+void STM_EVAL_LEDOff(Led_TypeDef Led);
+void STM_EVAL_LEDToggle(Led_TypeDef Led);
+void STM_EVAL_PBInit(Button_TypeDef Button, ButtonMode_TypeDef Button_Mode);
+uint32_t STM_EVAL_PBGetState(Button_TypeDef Button);
+void STM_EVAL_COMInit(COM_TypeDef COM, USART_InitTypeDef* USART_InitStruct);
